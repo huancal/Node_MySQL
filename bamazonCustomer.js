5aca8007fa875a8d542e3c053ee730b7ef093a1f
Huan Nguyen <huancal1984@gmail.com>
2019-12-13 13:55:35 -0800

    js logic 90 percnet

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,7 +15,7 @@ connection.connect(err => {
 
     console.log(`You have been connected to threadID: ${connection.threadID}`);
     makeTable();
-    connection.end();
+    // connection.end();
 });
 
 var makeTable = function () {
@@ -24,16 +24,45 @@ var makeTable = function () {
             console.log(res[i].ITEM_ID + " || " + res[i].PRODUCT_NAME + " || " +
                 res[i].DEPARTMENT_NAME + " || " + res[i].PRICE + " || " + res[i].STOCK_QUANTITY + "\n ");
         }
-        promptCustomer(res);
+        userPrompt();
     })
 }
 
-var promptCustomer = function (res) {
+userPrompt = () => {
     inquirer.prompt([{
         type: 'input',
-        name: 'choice',
-        message: 'What would like you to purchase? [Quit with Q]'
-    }]).then(function (answer) {
+        message: 'What is the ID of the item you would like to buy?',
+        name: 'itemID'
+    }]).then(answers => {
+        answers.itemID;
+        console.log(answers.itemID);
+        connection.query("SELECT * FROM PRODUCTS WHERE ITEM_ID = ?", [answers.itemID], function (err, res) {
+            // console.log(res);
+            var itemInventory = res[0].STOCK_QUANTITY;
+            stockPrompt(itemInventory, answers.itemID);
+        })
+    })
+}
 
+stockPrompt = (itemInventory, ITEM_ID) => {
+    inquirer.prompt([{
+        type: 'input',
+        message: 'how many units would you like to buy?',
+        name: 'unitstobuy'
+    }]).then(answers => {
+        answers.unitstobuy
+        // console.log(answers.stockID);
+        if (answers.unitstobuy > itemInventory)
+            console.log("sorry out of stock!");
+        else {
+            var quantityLeft = itemInventory - answers.unitstobuy;
+            connection.query("UPDATE PRODUCTS SET STOCK_QUANTITY = ? WHERE ITEM_ID = ?", [quantityLeft, ITEM_ID], function (err, res) {
+                if (err) throw err;
+                console.log(err);
+                console.log(res);
+                console.log('order placed!');
+                makeTable();
+            })
+        }
     })
 }
